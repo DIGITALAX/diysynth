401f9ef1aaaa79da2672a92620b7a4479b7f6b86
emmajane1313 <admin@digitalax.xyz>
2022-09-28 06:51:29 -0400

    twitter card

--- theme.config.js
+++ theme.config.js
@@ -53,8 +53,8 @@ export default {
         <meta name="og:image" content="https://diysynth.xyz/card.png" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:site" content="@digitalax_"/>
-        <meta name="twitter:site" content="https://diysynth.xyz/" />
-        <meta name="twitter:image:src" content="https://diysynth.xyz/card.png" />
+        <meta name="twitter:creator" content="@digitalax_">
+        <meta name="twitter:image" content="https://diysynth.xyz/card.png" />
 
 
         {/* Misc */}

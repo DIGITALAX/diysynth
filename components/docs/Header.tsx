96b1bb157868d3b4308313ca90665476e68938a4
emmajane1313 <admin@digitalax.xyz>
2022-09-28 07:18:13 -0400

    refactor

--- components/docs/Header.tsx
+++ components/docs/Header.tsx
@@ -15,18 +15,11 @@ export function Header() {
   return (
     <header className="mb-10 flex flex-col items-center">
       <div className="mt-8 w-auto h-12 md:h-14 text-center">
-        {!systemTheme || systemTheme === "dark" ? (
-          <h1 className="text-7xl font-sourceBold text-offBlack">DIY SYNTH</h1>
-        ) : (
-          <h1 className="text-7xl font-sourceBold text-offWhite">DIY SYNTH</h1>
-        )}
+          <h1 className="text-7xl font-sourceBold">DIY SYNTH</h1>
       </div>
-      {!systemTheme || systemTheme === "dark" ? (
-      <p className="md:text-center pt-28 md:p-10 md:pb-2 font-sourceLight text-base text-gray-500 md:!text-2xl text-offBlack">
+      <p className="md:text-center pt-28 md:p-10 md:pb-2 font-sourceLight text-base text-gray-500 md:!text-2xl">
         The Latent Codex for Image Synthesis Research & Prompt Engineers
-      </p>) : (<p className="md:text-center pt-28 md:p-10 md:pb-2 font-sourceLight text-base text-gray-500 md:!text-2xl text-offWhite">
-        The Latent Codex for Image Synthesis Research & Prompt Engineers
-      </p>)}
+      </p>
     </header>
   );
 }
